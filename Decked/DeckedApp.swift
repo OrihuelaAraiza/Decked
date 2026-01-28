f02902444652b753b3e019d8e9607199c2f2b393
Jp Orihuela <112457674+OrihuelaAraiza@users.noreply.github.com>
2026-01-27 18:12:13 -0600

    Initial Commit

---
+++ Decked/DeckedApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeckedApp.swift
+//  Decked
+//
+//  Created by Juan Pablo Orihuela Araiza on 27/01/26.
+//
+
+import SwiftUI
+
+@main
+struct DeckedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DeckedTests/DeckedTests.swift
@@ -0,0 +1,17 @@
+//
+//  DeckedTests.swift
+//  DeckedTests
+//
+//  Created by Juan Pablo Orihuela Araiza on 27/01/26.
+//
+
+import Testing
+@testable import Decked
+
+struct DeckedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
